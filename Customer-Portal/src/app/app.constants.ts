bc7f2671acc2936692a46fc968c5091e5eb2801d
minefuture09 <minefuture09@gmail.com>
2023-03-25 13:25:57 +0530

    first commit

---
+++ Customer-Portal/src/app/app.constants.ts
@@ -0,0 +1,10 @@
+// export const API_URL = "http://finalauthorization.us-east-1.elasticbeanstalk.com"
+// export const CN_URL = "http://CalculateNetWorth.us-west-2.elasticbeanstalk.com"
+// export const MF_URL = "http://DailyMutualFund.us-west-2.elasticbeanstalk.com"
+// export const SD_URL = "http://dailyshareprice.us-east-1.elasticbeanstalk.com"
+
+
+export const API_URL = "http://localhost:8081"
+export const CN_URL = "http://localhost:8084"
+export const MF_URL = "http://localhost:8083"
+export const SD_URL = "http://localhost:8082"
---
+++ Customer-Portal/src/app/components/menu/menu.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+ public loggedIn:boolean=false;
+  constructor(private loginService:LoginService) { }
+
+  ngOnInit(): void {
+    this.loggedIn=this.loginService.isLoggedIn();
+  
+  }
+
+  logoutUser()
+  {
+    localStorage.removeItem("token");
+  }
+}
